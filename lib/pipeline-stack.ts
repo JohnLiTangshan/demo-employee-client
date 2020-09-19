1d94a5aa1485c19d152c64fdf1fbe16c13f01758
John Li <xuepeng.li1@pactera.com>
2020-09-19 14:52:16 +0800

    add pipeline

---
+++ lib/pipeline-stack.ts
@@ -0,0 +1,35 @@
+import { Stack, SecretValue, Construct, StackProps } from '@aws-cdk/core';
+import * as codepipeline from '@aws-cdk/aws-codepipeline';
+import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
+import { CdkPipeline, SimpleSynthAction } from '@aws-cdk/pipelines';
+
+export class PipelineStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const OWNER = this.node.tryGetContext("repoOwner");
+    const REPO = this.node.tryGetContext("repo");
+    
+    const sourceArtifact = new codepipeline.Artifact();
+    const cloudAssemblyArtifact = new codepipeline.Artifact();
+
+    const pipeline = new CdkPipeline(this, 'Pipeline', {
+      pipelineName: 'MyAppPipeline',
+      cloudAssemblyArtifact,
+
+      sourceAction: new codepipeline_actions.GitHubSourceAction({
+        actionName: 'GitHub',
+        output: sourceArtifact,
+        oauthToken: SecretValue.secretsManager('githubToken'),
+        trigger: codepipeline_actions.GitHubTrigger.POLL,
+        owner: OWNER,
+        repo: REPO,
+      }),
+      synthAction: SimpleSynthAction.standardNpmSynth({
+        sourceArtifact,
+        cloudAssemblyArtifact,
+        buildCommand: 'npm run build',
+      })
+    });
+  }
+}
