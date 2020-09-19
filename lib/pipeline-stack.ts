6ac4734b0f8adf6636c20c8eedade6f0f463a4a4
John Li <xuepeng.li1@pactera.com>
2020-09-19 20:11:57 +0800

    add pipeline

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,7 +1,10 @@
 import { Stack, SecretValue, Construct, StackProps } from '@aws-cdk/core';
 import * as codepipeline from '@aws-cdk/aws-codepipeline';
 import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
+import * as codebuild from '@aws-cdk/aws-codebuild';
 import { CdkPipeline, SimpleSynthAction } from '@aws-cdk/pipelines';
+import { Bucket } from '@aws-cdk/aws-s3';
+import { CodeBuildAction } from '@aws-cdk/aws-codepipeline-actions';
 
 export class PipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -9,9 +12,18 @@ export class PipelineStack extends Stack {
 
     const OWNER = this.node.tryGetContext("repoOwner");
     const REPO = this.node.tryGetContext("repo");
-    
+    const webSiteBucketName = this.node.tryGetContext('websiteBucketName');
+
     const sourceArtifact = new codepipeline.Artifact();
     const cloudAssemblyArtifact = new codepipeline.Artifact();
+
+    const webSiteBucket = new Bucket(this, 'WebsiteBucket', {
+        bucketName: webSiteBucketName,
+        websiteIndexDocument: 'index.html',
+        websiteErrorDocument: 'index.html',
+        publicReadAccess: true
+    });
+
 
     const pipeline = new CdkPipeline(this, 'Pipeline', {
       pipelineName: 'DemoEmployeeClientPipeline',
@@ -31,6 +43,66 @@ export class PipelineStack extends Stack {
         buildCommand: 'npm run build',
       })
     });
+
+    // Unit Test
+    const unitTestStage = pipeline.addStage('Unit Test');
+    
+    const unitTestProject = new codebuild.PipelineProject(this, 'UnitTestProject', {
+      buildSpec: codebuild.BuildSpec.fromSourceFilename('buildspec-unittest.json'),
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_3_0
+      },
+      projectName: 'DemoEmployeeClientUnitTest'
+    });
+
+    const unitTestAction: CodeBuildAction = new codepipeline_actions.CodeBuildAction({
+      actionName: 'Unit Test',
+      input: sourceArtifact,
+      project: unitTestProject
+    });
+
+    unitTestStage.addActions(unitTestAction);
+
+    // E2E Test
+    const e2eTestStage = pipeline.addStage('E2E Test');
+    const e2eTestProject = new codebuild.PipelineProject(this, 'E2ETestProject', {
+      buildSpec: codebuild.BuildSpec.fromSourceFilename('buildspec-e2etest.json'),
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_3_0
+      },
+      projectName: 'DemoEmployeeClientE2ETest'
+    });
+    const e2eTestAction: CodeBuildAction = new codepipeline_actions.CodeBuildAction({
+      actionName: 'E2E Test',
+      input: sourceArtifact,
+      project: e2eTestProject
+    });
+    e2eTestStage.addActions(e2eTestAction);
+
+    // Deploy
+    const deployStage = pipeline.addStage('Deploy');
+    const deployProject = new codebuild.PipelineProject(this, 'Deploy', {
+      buildSpec: codebuild.BuildSpec.fromSourceFilename('buildspec-deploy.json'),
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_3_0
+      },
+      projectName: 'DemoEmployeeClientDeploy'
+    });
+
+    const deployArtifact = new codepipeline.Artifact();
+    const deployAction: CodeBuildAction = new codepipeline_actions.CodeBuildAction({
+      actionName: 'Deploy',
+      input: sourceArtifact,
+      outputs: [deployArtifact],
+      project: deployProject,
+      environmentVariables: {
+        WEBSITE_BUCKET: {value: webSiteBucket.bucketName}
+      },
+    });
+    deployStage.addActions(deployAction);
+
+
+
     
   }
 }
